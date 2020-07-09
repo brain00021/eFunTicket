e724329ff8cf2b26138098fafc879108a2718384
hq.brian.li <brain00021@gmail.com>
2020-07-10 00:24:18 +0800

    update banner

--- js/index.js
+++ js/index.js
@@ -1,3 +1,11 @@
 $(function() {
- 
+  function tabSetting(e) {
+    e.stopPropagation();
+    let showContent = this.getAttribute('data-selection');
+    debugger;
+    $(this).addClass('active').siblings('li').removeClass('active');
+    let offset = $("#"+showContent).offset();
+    console.log(offset);
+  }
+  $(document).on('click', '.nav-links a', tabSetting);
 });
