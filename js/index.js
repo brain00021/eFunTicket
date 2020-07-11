de952bf6dba4f28adb940905d4b16573db7715ae
hq.brian.li <brain00021@gmail.com>
2020-07-11 10:17:53 +0800

    banner setting finished

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
 $(function() {
   function tabSetting(e) {
     e.stopPropagation();
+    e.preventDefault();
     let showContent = this.getAttribute('data-selection');
-    debugger;
     $(this).addClass('active').siblings('li').removeClass('active');
     let offset = $("#"+showContent).offset();
-    console.log(offset);
+    $("html, body").animate({ scrollTop: offset.top-50 }, 1000);
   }
   $(document).on('click', '.nav-links a', tabSetting);
 });
