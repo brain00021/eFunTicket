1279083b4f3d4b3bb5473042bf51add8e4b5a979
hq.brian.li <brain00021@gmail.com>
2020-07-13 00:48:11 +0800

    update all platform

--- js/index.js
+++ js/index.js
@@ -8,4 +8,17 @@ $(function() {
     $("html, body").animate({ scrollTop: offset.top-50 }, 1000);
   }
   $(document).on('click', '.nav-links a', tabSetting);
+  //reset 
+  $('body').css('overflow', 'auto');
+  $('.dialog-bg').fadeOut();
+  //dialog 設定
+  $(document).on('click', '.dailog-btn,.dialog-bg',function(){
+    $('body').css('overflow', 'auto');
+    $('.dialog-bg').fadeOut();
+  })
+  $(document).on('click', '#btn-detail',function(){
+    $('body').css('overflow', 'hidden');
+    $('.dialog-bg').fadeIn();
+  })
+
 });
